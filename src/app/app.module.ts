7054c226f9c19966f7d299c198bb49db268877dc
Moshik Alper <moshikalper@gmail.com>
2025-12-02 12:35:06 +0200

    Add todo list and preview cmps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './cmps/app-header/app-header.component';
+import { TodoListComponent } from './cmps/todo-list/todo-list.component';
+import { TodoPreviewComponent } from './cmps/todo-preview/todo-preview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    TodoListComponent,
+    TodoPreviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cmps/todo-preview/todo-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-preview',
+  standalone: false,
+  templateUrl: './todo-preview.component.html',
+  styleUrl: './todo-preview.component.scss'
+})
+export class TodoPreviewComponent {
+
+}
