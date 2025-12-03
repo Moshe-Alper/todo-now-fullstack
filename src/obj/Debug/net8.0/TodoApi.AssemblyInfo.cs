0c41f120a784a06c03a1e7b2fff29e701b304d60
Moshik Alper <moshikalper@gmail.com>
2025-12-03 11:54:20 +0200

    Delete current readme file

--- src/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a5a1dfff592cea73a3131a012fd0ffb18203106")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2660a4ad82633afb010442e30de7e66c0a9db86")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
