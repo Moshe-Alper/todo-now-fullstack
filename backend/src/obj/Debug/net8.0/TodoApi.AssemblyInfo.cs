5d0bda1fd07c6f3cdddc033fa9f0ad11204564df
Moshik Alper <moshikalper@gmail.com>
2026-01-10 19:38:53 +0200

    Update assembly informational version in TodoApi.AssemblyInfo.cs and refresh cache file

--- backend/src/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/src/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eed286e78e4ef9b4ea2da969ac1da88373544f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c358ee39ce42696029d30de23e3c55651eab0a50")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
